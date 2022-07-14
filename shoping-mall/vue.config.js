c66c270fd64a1bd75522e566f0061f7f9a0276bb
sunjiayi <sunjiayibfsu@sina.com>
2022-07-14 14:35:03 +0800

    add: 添加devServer api接口地址

--- shoping-mall/vue.config.js
+++ shoping-mall/vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭eslint自动校验工具
-  lintOnSave: false
+  lintOnSave: false,
+  devServer:{
+    proxy:{
+      "/api":{
+        target: "http://gmall-h5-api.atguigu.cn",
+      }
+    }
+  }
 })

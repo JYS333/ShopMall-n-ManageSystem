1d24e39e29b9763c2f6d2bb79109609ede7ad6ad
sunjiayi <sunjiayibfsu@sina.com>
2022-07-14 13:35:15 +0800

    add: 商品Home首页静态页面搭建完成

--- shoping-mall/src/pages/Home/index.vue
+++ shoping-mall/src/pages/Home/index.vue
@@ -6,6 +6,9 @@
     <ListContainer />
     <Recommend />
     <Rank />
+    <Like />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
@@ -13,9 +16,13 @@
 import ListContainer from "@/pages/Home/ListContainer";
 import Recommend from "@/pages/Home/Recommend";
 import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
   name: "Home",
-  components: { ListContainer, Recommend, Rank },
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
 };
 </script>
 

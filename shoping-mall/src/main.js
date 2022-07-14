635d0c19565d70050dcf674b430ba5083622d5fd
sunjiayi <sunjiayibfsu@sina.com>
2022-07-14 13:20:23 +0800

    add: 新增Home组件内部TypeNav ListContent Recommend Rank组件

--- shoping-mall/src/main.js
+++ shoping-mall/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 // 引入路由
 import router from '@/router'
 
+// 引入三级联动组件，为全局组件
+import TypeNav from '@/pages/Home/TypeNav';
+
+// 第一个参数为组件名称，第二个参数为组件
+Vue.component(TypeNav.name, TypeNav);
+
 Vue.config.productionTip = false
 
 new Vue({
--- shoping-mall/src/pages/Home/index.vue
+++ shoping-mall/src/pages/Home/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Home</div>
+  <div>
+    <!-- TypeNav是全局组件，在main.js里引入 -->
+    <TypeNav />
+    <!-- 下面两个是内部组件 -->
+    <ListContainer />
+    <Recommend />
+    <Rank />
+  </div>
 </template>
 
 <script>
-export default {};
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+export default {
+  name: "Home",
+  components: { ListContainer, Recommend, Rank },
+};
 </script>
 
 <style></style>

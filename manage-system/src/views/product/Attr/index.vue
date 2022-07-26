d635e4b689367f2df86ff751a3008a9e023bf254
sunjiayi <sunjiayibfsu@sina.com>
2022-07-26 17:15:59 +0800

    add: 属性管理下面添加修改的静态页面

--- manage-system/src/views/product/Attr/index.vue
+++ manage-system/src/views/product/Attr/index.vue
@@ -6,72 +6,147 @@
     </el-card>
     <!-- 第二个卡片 -->
     <el-card>
-      <el-button
-        icon="el-icon-plus"
-        size="small"
-        type="primary"
-        style="margin-bottom: 20px"
-      >
-        添加属性
-      </el-button>
-      <!-- 表格：展示属性列表数据 -->
-      <el-table :data="attrList" style="width: 100%" border>
-        <!-- type:index prop:prop 序号 -->
-        <el-table-column
-          type="index"
-          prop="prop"
-          label="序号"
-          width="80"
-          align="center"
+      <!-- 属性列表 及 上方button -->
+      <div v-show="isShowTable">
+        <el-button
+          icon="el-icon-plus"
+          size="small"
+          type="primary"
+          style="margin-bottom: 20px"
+          :disabled="ids.category3Id ? false : true"
+          @click="isShowTable = false"
         >
-        </el-table-column>
-        <!-- prop:attrName 属性名称 -->
-        <el-table-column
-          prop="attrName"
-          label="属性名称"
-          width="300"
-          align="center"
+          添加属性
+        </el-button>
+        <!-- 表格：展示属性列表数据 -->
+        <el-table :data="attrList" style="width: 100%" border>
+          <!-- type:index prop:prop 序号 -->
+          <el-table-column
+            type="index"
+            prop="prop"
+            label="序号"
+            width="80"
+            align="center"
+          >
+          </el-table-column>
+          <!-- prop:attrName 属性名称 -->
+          <el-table-column
+            prop="attrName"
+            label="属性名称"
+            width="300"
+            align="center"
+          >
+          </el-table-column>
+          <!-- prop:attrName 属性值列表 -->
+          <el-table-column
+            prop="attrValueList"
+            label="属性值列表"
+            width="width"
+            align="left"
+          >
+            <template slot-scope="{ row }">
+              <el-tag
+                v-for="(attr, index) in row.attrValueList"
+                :key="index"
+                style="margin-right: 10px"
+              >
+                {{ attr.valueName }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="300" align="center">
+            <!-- row就是对应的那一行的品牌信息 -->
+            <template>
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="isShowTable = false"
+              >
+                编辑
+              </el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页器 -->
+        <el-pagination
+          style="margin-top: 20px; text-align: center"
+          :current-page="page"
+          :total="total"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="limit"
+          layout="prev, pager, next, jumper, ->, sizes, total"
         >
-        </el-table-column>
-        <!-- prop:attrName 属性值列表 -->
-        <el-table-column
-          prop="attrValueList"
-          label="属性值列表"
-          width="width"
-          align="left"
+        </el-pagination>
+      </div>
+      <!-- 修改属性form -->
+      <div v-show="!isShowTable">
+        <el-form inline>
+          <el-form-item label="属性名称">
+            <el-input placeholder="请输入属性名"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button icon="el-icon-plus" type="primary"> 添加 </el-button>
+            <el-button> 取消 </el-button>
+          </el-form-item>
+        </el-form>
+        <el-table
+          :data="attrList"
+          style="width: 100%; margin-bottom: 20px"
+          border
         >
-          <template slot-scope="{ row }">
-            <el-tag
-              v-for="(attr, index) in row.attrValueList"
-              :key="index"
-              style="margin-right: 10px"
-            >
-              {{ attr.valueName }}
-            </el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="prop" label="操作" width="300" align="center">
-          <!-- row就是对应的那一行的品牌信息 -->
-          <template>
-            <el-button type="warning" icon="el-icon-edit" size="mini">
-              编辑
-            </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini">
-              删除
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- 分页器 -->
-      <el-pagination
-        style="margin-top: 20px; text-align: center"
-        :current-page="page"
-        :total="total"
-        :page-sizes="[5, 10, 15, 20]"
-        :page-size="limit"
-        layout="prev, pager, next, jumper, ->, sizes, total"
-      >
-      </el-pagination>
+          <!-- type:index prop:prop 序号 -->
+          <el-table-column
+            type="index"
+            prop="prop"
+            label="序号"
+            width="80"
+            align="center"
+          >
+          </el-table-column>
+          <!-- prop:attrName 属性名称 -->
+          <el-table-column prop="prop" label="属性名称" width="width">
+          </el-table-column>
+          <!-- prop:attrName 属性值列表 -->
+          <!-- <el-table-column
+            prop="prop"
+            label="属性值列表"
+            width="width"
+            align="left"
+          >
+            <template slot-scope="{ row }">
+              <el-tag
+                v-for="(attr, index) in row.attrValueList"
+                :key="index"
+                style="margin-right: 10px"
+              >
+                {{ attr.valueName }}
+              </el-tag>
+            </template>
+          </el-table-column> -->
+          <el-table-column prop="prop" label="操作" width="300" align="center">
+            <!-- row就是对应的那一行的品牌信息 -->
+            <template>
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="isShowTable = false"
+              >
+                编辑
+              </el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -84,6 +159,12 @@ export default {
       limit: 5, // 当前页数展示条数
       total: 0, // 数据总数
       attrList: [], // 展示的列表信息
+      isShowTable: false, // 是否显示表格
+      ids: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: "",
+      },
     };
   },
   methods: {
@@ -97,7 +178,8 @@ export default {
       }
     },
     getCategoryId(data) {
-      // 自定义时间回调，data是子组件给父组件传递的数据，
+      // 自定义时间回调，data是子组件给父组件传递的数据
+      this.ids = { ...data };
       this.getAttrList(data);
     },
   },
